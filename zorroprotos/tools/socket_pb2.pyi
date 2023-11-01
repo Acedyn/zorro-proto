a1647ffe2b6f457b674505925edf493049fd68ea
Lambin Simon <slambin@gmail.com>
2023-11-01 14:35:54 +0100

    feat: make the socket recursive

--- zorroprotos/tools/socket_pb2.pyi
+++ zorroprotos/tools/socket_pb2.pyi
@@ -5,29 +5,23 @@ from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Opti
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
-class SocketEntry(_message.Message):
-    __slots__ = ["kind", "json"]
-    KIND_FIELD_NUMBER: _ClassVar[int]
-    JSON_FIELD_NUMBER: _ClassVar[int]
-    kind: str
-    json: bytes
-    def __init__(self, kind: _Optional[str] = ..., json: _Optional[bytes] = ...) -> None: ...
-
 class Socket(_message.Message):
-    __slots__ = ["link", "raw", "descriptor", "entries"]
+    __slots__ = ["kind", "raw", "json", "descriptor", "entries"]
     class EntriesEntry(_message.Message):
         __slots__ = ["key", "value"]
         KEY_FIELD_NUMBER: _ClassVar[int]
         VALUE_FIELD_NUMBER: _ClassVar[int]
         key: str
-        value: SocketEntry
-        def __init__(self, key: _Optional[str] = ..., value: _Optional[_Union[SocketEntry, _Mapping]] = ...) -> None: ...
-    LINK_FIELD_NUMBER: _ClassVar[int]
+        value: Socket
+        def __init__(self, key: _Optional[str] = ..., value: _Optional[_Union[Socket, _Mapping]] = ...) -> None: ...
+    KIND_FIELD_NUMBER: _ClassVar[int]
     RAW_FIELD_NUMBER: _ClassVar[int]
+    JSON_FIELD_NUMBER: _ClassVar[int]
     DESCRIPTOR_FIELD_NUMBER: _ClassVar[int]
     ENTRIES_FIELD_NUMBER: _ClassVar[int]
-    link: str
+    kind: str
     raw: bytes
+    json: bytes
     descriptor: str
-    entries: _containers.MessageMap[str, SocketEntry]
-    def __init__(self, link: _Optional[str] = ..., raw: _Optional[bytes] = ..., descriptor: _Optional[str] = ..., entries: _Optional[_Mapping[str, SocketEntry]] = ...) -> None: ...
+    entries: _containers.MessageMap[str, Socket]
+    def __init__(self, kind: _Optional[str] = ..., raw: _Optional[bytes] = ..., json: _Optional[bytes] = ..., descriptor: _Optional[str] = ..., entries: _Optional[_Mapping[str, Socket]] = ...) -> None: ...
